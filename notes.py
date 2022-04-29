7b524ebe6af763fe322a277ac0f4fbafab00e46f
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-04-29 12:09:39 -0400

    functions with outputs - notes

--- notes.py
+++ notes.py
@@ -1,3 +1,19 @@
  #100DaysofCode - Day 10 Beginner Functions with Outputs
 
+#Functions with Outputs
+# def my_function():
+#     result = 3 *2
+#     return result
+#     output = result
 
+# output = my_function()
+# print(output)
+
+
+def format_name(f_name, l_name):
+    formatted_f_name = f_name.title()
+    formatted_l_name = l_name.title()
+    return(f" {formatted_f_name} {formatted_l_name}")
+
+formatted_string = format_name("shAUN", "DAWSON")
+print(formatted_string)
