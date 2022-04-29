b06ad1840875eb08c4c72ad1b1db963c1894f67c
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-04-29 13:04:26 -0400

    docstrings - notes

--- notes.py
+++ notes.py
@@ -31,28 +31,43 @@
 # print(format_name(input("what is your first name? "), input("what is your last name? ")))
 
 
+
 #EXERCISE - DAYS IN MONTH
-def is_leap(year):
-  if year % 4 == 0:
-    if year % 100 == 0:
-      if year % 400 == 0:
-        return True
-      else:
-        return False
-    else:
-      return True
-  else:
-    return False
+# def is_leap(year):
+#   if year % 4 == 0:
+#     if year % 100 == 0:
+#       if year % 400 == 0:
+#         return True
+#       else:
+#         return False
+#     else:
+#       return True
+#   else:
+#     return False
 
-def days_in_month(year, month):
-    if month > 12 or month < 1:
-        return "Invalid month"
-    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    if is_leap(year) and month == 2:
-        return 29
-    return month_days[month -1]
+# def days_in_month(year, month):
+#     if month > 12 or month < 1:
+#         return "Invalid month"
+#     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+#     if is_leap(year) and month == 2:
+#         return 29
+#     return month_days[month -1]
 
-year = int(input("Enter a year: "))
-month = int(input("Enter a month: "))
-days = days_in_month(year, month)
-print(days)
+# year = int(input("Enter a year: "))
+# month = int(input("Enter a month: "))
+# days = days_in_month(year, month)
+# print(days)
+
+
+#DOCSTRINGS
+#Already used functions with outputs
+# length = len(formatted_name)
+
+#Return as an early exit
+def format_name(f_name, l_name):
+    """" Take a first and last name and format it to return the title case version of the name. """
+    if f_name == "" or l_name == "":
+        return "You didn't provide valid inputs."
+    formatted_f_name = f_name.title()
+    formatted_l_name = l_name.title()
+    return f"Result: {formatted_f_name} {formatted_l_name}"
