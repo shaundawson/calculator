afd23ee9059e7e080f63259bf5cc483fb4a1a230
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-04-29 12:20:09 -0400

    multiple return values - notes

--- notes.py
+++ notes.py
@@ -10,10 +10,22 @@
 # print(output)
 
 
+# def format_name(f_name, l_name):
+#     formatted_f_name = f_name.title()
+#     formatted_l_name = l_name.title()
+#     return(f" {formatted_f_name} {formatted_l_name}")
+
+# formatted_string = format_name("shAUN", "DAWSON")
+# print(formatted_string)
+
+
+#MULTIPLE RETURN VALUES
 def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You didn't provide a valid input"  
     formatted_f_name = f_name.title()
     formatted_l_name = l_name.title()
-    return(f" {formatted_f_name} {formatted_l_name}")
+    return(f"Result: {formatted_f_name} {formatted_l_name}")
+    print("This got printed")
 
-formatted_string = format_name("shAUN", "DAWSON")
-print(formatted_string)
+print(format_name(input("what is your first name? "), input("what is your last name? ")))
